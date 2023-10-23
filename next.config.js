3ca0bcc7bdb5ecde58bfc7bdb8ccd5611fcb3b03
Victor Aiyeola <aiyeolavictor@gmail.com>
2023-10-23 10:06:01 +0100

    chore: migrate example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/examples/landing' : '/',
+};
